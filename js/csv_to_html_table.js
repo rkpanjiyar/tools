baf9001e7ea5433e5beccba090a8b562add53e8f
amanbindal <bindal.aman@gmail.com>
2025-02-28 11:33:09 -0500

    Update csv_to_html_table.js
    Signed-off-by: amanbindal <bindal.aman@gmail.com>

--- js/csv_to_html_table.js
+++ js/csv_to_html_table.js
@@ -16,7 +16,7 @@ CsvToHtmlTable = {
             customTemplates[colIdx] = func;
         });
 
-        var $table = $("<table style='border-collapse: collapse;width: 100%;table-layout: auto;' class='table-container' id='" + el + "-table'></table>");
+        var $table = $("<table style='border-collapse: collapse;width: 100%;table-layout: auto;' class='table-container table table-striped table-condensed' id='" + el + "-table'></table>");
         var $containerElement = $("#" + el);
         $containerElement.empty().append($table);
 
